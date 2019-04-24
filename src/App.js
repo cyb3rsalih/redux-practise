500725aecc26861417bf66a91284e76d5a9e22d7
mehmetsalihbindak <mehmetsalihbindak@gmail.com>
2019-04-24 09:25:43 +0300

    files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
-import logo from './logo.svg';
+import {connect} from 'react-redux'
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+
+  onInc = () => {
+    this.props.dispatch({type:'INC'})
+  }
+
+  onDec = () => {
+    this.props.dispatch({type:'DEC'})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <h1>{this.props.count}</h1>
+        <button onClick={this.onInc}>+</button>
+        <button onClick={this.onDec}>-</button>
+      </div>
+    );
+  }
+
+  
+
 }
 
-export default App;
+const mapStateToProps = ({count}) => {
+    return {
+      count
+    }
+}
+export default connect(mapStateToProps)(App);
